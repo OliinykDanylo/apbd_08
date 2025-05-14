912a3a6c075ae405bd2bfd4d2f1e7eaed8e48fee
danylooliinyk <danylooliinyk.dev@gmail.com>
2025-05-14 17:40:32 +0200

    update

--- src/DevicesManager.Models/EmptyBatteryException.cs
+++
@@ -1,8 +0,0 @@
-namespace DevicesManager;
-
-public class EmptyBatteryException : Exception
-{
-    public EmptyBatteryException() 
-        : base("Battery level is too low to turn it on.") 
-    { }
-}
